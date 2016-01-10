54875c03a370cdac70537e989c2f9103ddd3bec1
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-10 10:49:10 -0800

    moved response to requestHandlers

--- router.js
+++ router.js
@@ -13,12 +13,15 @@
 exports.route = route;
 */
 
-function route(handle, pathname) {
+function route(handle, pathname, response) {
   console.log('About to route a request for: '+pathname);
   if(typeof handle[pathname]==='function'){
-    return handle[pathname]();
+    return handle[pathname](response);
   } else{
     console.log('no handler found for '+pathname);
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('404 not found');
+    response.end();
   }
 }
 module.exports = route;
